74baeb94188f9cf28c730cd563295a4d6af43346
Nhut Anh <nhutanh1605@gmail.com>
2023-11-11 21:35:27 +0700

    first comit

---
+++ src/main/java/polyshare/polyshare/PolyshareApplication.java
@@ -0,0 +1,13 @@
+package polyshare.polyshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/polyshare/polyshare/PolyshareApplicationTests.java
@@ -0,0 +1,13 @@
+package polyshare.polyshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
